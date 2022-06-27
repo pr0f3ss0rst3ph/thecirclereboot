cd804e1981a190c992a23ed54c206db821c023da
Stephon Fraser <43830615+pr0f3ss0rst3ph@users.noreply.github.com>
2022-06-26 21:04:02 -0400

    Styled Featured Blog and Added Subscribercard

--- frontend/src/components/Subscribecard.js
+++ frontend/src/components/Subscribecard.js
@@ -3,10 +3,12 @@ import React from "react";
 const Subscribecard = () => {
   return (
     <div className="subscribecard">
-      <div className="subscribe-site-title"></div>
-      <div className="subscribe-message"></div>
+      <div className="subscribe-site-title">THE TECH KLUB</div>
+      <div className="subscribe-message">
+        SUBSCRIBE TO OUR NEWSLETTER AND KEEP IN THE LOOP
+      </div>
       <div className="subscribe-input-box"></div>
-      <div className="subscribe-button"></div>
+      <div className="subscribe-button">Subscribe</div>
     </div>
   );
 };
