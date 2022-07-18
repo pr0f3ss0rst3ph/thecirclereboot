99d2aeab9e8412d238de903a78ff1df24a05439f
Stephon Fraser <43830615+pr0f3ss0rst3ph@users.noreply.github.com>
2022-07-17 22:46:04 -0400

    Completed Subscribe card styling

--- frontend/src/components/Subscribecard.js
+++ frontend/src/components/Subscribecard.js
@@ -7,8 +7,18 @@ const Subscribecard = () => {
       <div className="subscribe-message">
         SUBSCRIBE TO OUR NEWSLETTER AND KEEP IN THE LOOP
       </div>
-      <div className="subscribe-input-box"></div>
-      <div className="subscribe-button">Subscribe</div>
+      <form className="subscribe-form">
+        <div className="subscribe-input-box">
+          <input
+            type="text"
+            placeholder="Enter email here..."
+            id="subscribe-email"
+          />
+        </div>
+        <div className="subscribe-button-field">
+          <button className="subscribe-button">SUBSCRIBE</button>
+        </div>
+      </form>
     </div>
   );
 };
